0f350197f06b2d5e96151adf68066ccd5795f162
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-06-24 23:42:07 +0900

    Static fix

--- tenebrios_api/settings.py
+++ tenebrios_api/settings.py
@@ -129,7 +129,7 @@ APPEND_SLASH=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
